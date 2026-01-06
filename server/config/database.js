d3e5773f435dbfcec1bd94c62adc02daa2ffe02c
Yuvraj Karna <yuvraj.karna@iquadra.com>
2026-01-06 17:30:23 +0530

    feat: updated with .env variables

--- server/config/database.js
+++ server/config/database.js
@@ -1,8 +1,8 @@
 const { createClient } = require('@supabase/supabase-js');
 
 const supabase = createClient(
-  'https://ouslxpufrgmcequhfand.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im91c2x4cHVmcmdtY2VxdWhmYW5kIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU2NDMzNDEsImV4cCI6MjA4MTIxOTM0MX0.irLMePCLM8PlEYHLIe8GYMO8mGZgovvFgePAOjdVww8'
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_ANON_KEY
 );
 
 module.exports = supabase;
